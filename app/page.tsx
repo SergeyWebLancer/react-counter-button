ec43bf8d6820e351dfdfedc597f3e89287a697ad
SergeyDev <sergejdeveloper@gmail.com>
2025-02-27 11:58:05 +0200

    counter button all state

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
 "use client";
-import { useState } from "react"
+import { useState } from "react";
+import Button from "@/components/button";
 
 export default function Home() {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+      setCount(count + 1);
+  }
+
   return (
       <main className="flex min-h-screen flex-col items-center p-24 gap-4">
           <h1>React Counter Button</h1>
-          <Button />
+          <Button count={count} onClick={handleClick} />
+          <Button count={count} onClick={handleClick} />
       </main>
-  );
-}
-
-function Button() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-  
-  return (
-      <button 
-        onClick={handleClick}
-        className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2 cursor-pointer"
-      >
-        I have been clicked {count} times
-    </button>
   );
 }
